5249c09566f47b56f4586a56b62665bcd856f7e6
ytokumaru <ytokumaru16@gmail.com>
2024-03-06 20:41:05 +0900

    write more install scritps

---
+++ ubuntu/packages/cpp-dev.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+sudo apt install -y gcc
+sudo apt install -y g++
+sudo apt install -y clang-tools
+sudo apt install -y llvm
+sudo apt install -y cmake
+sudo apt install -y ninja
+
+sudo apt install -y ccache
+sudo apt install -y cppcheck
+sudo apt install -y valgrind
+sudo apt install -y gdb
+sudo apt install -y doxygen
+sudo apt install -y graphviz
--- ubuntu/packages/spack-deps.sh
+++ ubuntu/packages/spack-deps.sh
@@ -1,1 +1,26 @@
-sudo apt install -y build-essential ca-certificates coreutils curl environment-modules gfortran git gpg lsb-release python3 python3-distutils python3-venv unzip zip mercurial subversion gnupg2 zstd xz-utils bzip2 unzip gzip tar patch 
+#!/usr/bin/env bash
+
+sudo apt install -y build-essential
+sudo apt install -y ca-certificates
+sudo apt install -y coreutils
+sudo apt install -y curl
+sudo apt install -y environment-modules
+sudo apt install -y gfortran
+sudo apt install -y git
+sudo apt install -y gpg
+sudo apt install -y lsb-release
+sudo apt install -y python3
+sudo apt install -y python3-distutils
+sudo apt install -y python3-venv
+sudo apt install -y unzip
+sudo apt install -y zip
+sudo apt install -y mercurial
+sudo apt install -y subversion
+sudo apt install -y gnupg2
+sudo apt install -y zstd
+sudo apt install -y xz-utils
+sudo apt install -y bzip2
+sudo apt install -y unzip
+sudo apt install -y gzip
+sudo apt install -y tar
+sudo apt install -y patch
