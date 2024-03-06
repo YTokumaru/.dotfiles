c012005d70774414caf3c21a97a9ffdf2e1e55f0
ytokumaru <ytokumaru16@gmail.com>
2024-03-06 20:18:35 +0900

    Use spack as submodule

---
+++ ubuntu/packages/install-spack-deps.sh
@@ -0,0 +1,1 @@
+sudo apt install -y build-essential ca-certificates coreutils curl environment-modules gfortran git gpg lsb-release python3 python3-distutils python3-venv unzip zip mercurial subversion gnupg2 zstd xz-utils bzip2 unzip gzip tar patch 
