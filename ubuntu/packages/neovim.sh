38ddea491af655757606f61f143294279876b899
ytokumaru <ytokumaru16@gmail.com>
2024-03-28 21:55:06 +0900

    install dependencies of neovim

--- ubuntu/packages/neovim.sh
+++ ubuntu/packages/neovim.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
-sudo apt-get update 
+sudo apt-get update
 sudo apt-get install -y libfuse2
+sudo apt-get install -y ripgrep fd-find nodejs cargo
 
+# Install lazy-git
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+rm lazygit*
+
+# Install neovim with appimage
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
-
 sudo mkdir -p /opt/nvim
 sudo mv nvim.appimage /opt/nvim/nvim
