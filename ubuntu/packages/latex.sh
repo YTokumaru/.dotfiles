e13d69b4629f093effbd4596a48897c19f9d0d34
ytokumaru <ytokumaru16@gmail.com>
2024-03-29 11:26:04 +0900

    Add condition for WSL

--- ubuntu/packages/latex.sh
+++ ubuntu/packages/latex.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 sudo apt update
-sudo apt upgrade -y
 
 sudo apt install -y texlive-full
 
-code --install-extension James-Yu.latex-workshop
+# On WSL, visual studio code is installed
+if [[ ! -f "/sys/fs/binfmt_misc/WSLInterop" ]]; then
+	code --install-extension James-Yu.latex-workshop
+fi
+
