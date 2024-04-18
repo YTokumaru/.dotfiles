599b2318a2dda1e6d645d0c8d25176c74e551b8c
ytokumaru <ytokumaru16@gmail.com>
2024-04-18 23:19:51 +0900

    add color change to vscode

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,33 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- vscode-neovim configuration to change the color of the theme depending on the mode
+if vim.g.vscode then
+  vim.api.nvim_exec(
+    [[
+      " THEME CHANGER
+      function! SetCursorLineNrColorInsert(mode)
+          " Insert mode: blue
+          if a:mode == "i"
+              call VSCodeNotify('nvim-theme.insert')
+
+          " Replace mode: red
+          elseif a:mode == "r"
+              call VSCodeNotify('nvim-theme.replace')
+          endif
+      endfunction
+
+      augroup CursorLineNrColorSwap
+          autocmd!
+          autocmd ModeChanged *:[vV\x16]* call VSCodeNotify('nvim-theme.visual')
+          autocmd ModeChanged *:[R]* call VSCodeNotify('nvim-theme.replace')
+          autocmd InsertEnter * call SetCursorLineNrColorInsert(v:insertmode)
+          autocmd InsertLeave * call VSCodeNotify('nvim-theme.normal')
+          autocmd CursorHold * call VSCodeNotify('nvim-theme.normal')
+          autocmd ModeChanged [vV\x16]*:* call VSCodeNotify('nvim-theme.normal')
+      augroup END
+  ]],
+    false
+  )
+end
