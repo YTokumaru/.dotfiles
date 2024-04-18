37e4f6b91fed73bbf8f03a4a5abf295f47f915d2
ytokumaru <ytokumaru16@gmail.com>
2024-04-18 22:43:31 +0900

    add vscode keymaps

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- vscode-nvim specific keymaps
+if vim.g.vscode then
+    vim.keymap.set("n", "<C-/>", "<Cmd>call VSCodeNotify('workbench.action.terminal.toggleTerminal')<CR>", { desc = "Terminal (root dir)" })
+    vim.keymap.set("n", "<leader>gg", "<Cmd>call VSCodeNotify('workbench.view.scm')<CR>", { desc = "Source control" })
+    vim.keymap.set("n", "<leader>e", "<Cmd>call VSCodeNotify('workbench.view.explorer')<CR>", { desc = "Explorer" })
+    
+    -- tab-related shorcuts
+    vim.keymap.set("n", "H", "<Cmd>call VSCodeNotify('workbench.action.previousEditor')<CR>", { desc = "Go to previous tab" })
+    vim.keymap.set("n", "L", "<Cmd>call VSCodeNotify('workbench.action.nextEditor')<CR>", { desc = "Go to next tab" })
+    vim.keymap.set("n", "<leader>bd", "<Cmd>call VSCodeNotify('workbench.action.closeActiveEditor')<CR>", { desc = "Close current tab" })
+    vim.keymap.set("n", "<leader>br", "<Cmd>call VSCodeNotify('workbench.action.closeEditorsToTheRight')<CR>", { desc = "Close tabs to the right" })
+    vim.keymap.set("n", "<leader>bl", "<Cmd>call VSCodeNotify('workbench.action.closeEditorsToTheLeft')<CR>", { desc = "Close tabs to the left" })
+end
